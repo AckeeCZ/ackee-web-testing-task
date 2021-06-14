6eb1463f4c11cfd1b6e7908bc586b7f0585aeebf
Filip Kubik <filip.kubik@ackee.cz>
2021-06-14 16:25:57 +0200

    ✨ Show pages on home

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,19 @@
 import * as React from 'react';
+import { Link } from 'gatsby-plugin-intl';
+
+import config from '../config';
+
+const referencesRoutes = config.routes.references;
 
 export default function Component() {
-    return <h1>Yo</h1>;
+    return (
+        <>
+            <h1>Our case studies</h1>
+            {Object.values(referencesRoutes).map(route => (
+                <Link to={route}>
+                    <p>{route}</p>
+                </Link>
+            ))}
+        </>
+    );
 }
